e483b45ed04cd6eb359e11b8daf18aff48fe1fb6
ELionEli <elioneli1888@gmail.com>
2025-07-08 18:32:00 +0300

    Changes in the Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
 import type { FC } from "react";
+import { AuthenticationForm } from "../components/authentication-form/AuthenticationForm";
+import "../components/authentication-form/AuthenticationForm.css"
 
 interface IHomePageProps {}
 
@@ -7,15 +9,24 @@ export const HomePage: FC<IHomePageProps> = () => {
     <div
       style={{
         display: "flex",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
         height: "80vh",
         fontSize: "24px",
         color: "var(--primary-blue)",
-        textShadow: "0 0 6rem var(--primary-blue)"
+        textShadow: "0 0 6rem var(--primary-blue)",
+        gap: "10rem",
       }}
     >
-      <h1>Welcome to Social Media</h1>
+      <div style={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
+        <h1>Welcome to Social Media</h1>
+      </div>
+      <div style={{ flex: 1, display: "flex", justifyContent: "flex-start" }}>
+        <div style={{ width: "100%", maxWidth: 400 }}>
+          <AuthenticationForm mode="signin" />
+        </div>
+      </div>
     </div>
   );
 };
